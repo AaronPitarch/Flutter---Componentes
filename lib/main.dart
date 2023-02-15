5f31c2e9f4c874b229d3d0829a7884b4c598631e
AARON <aapitarch@gmail.com>
2023-02-15 11:46:37 +0100

    Crear ListTile

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'package:flutter/material.dart';
  
    @override
    Widget build(BuildContext context) {
-     return const MaterialApp(
+     return MaterialApp(
        title: 'Componentes App',
        debugShowCheckedModeBanner: false,
 
--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -1,32 +1,36 @@
 import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget {
-  const HomePageTemp({super.key});
+  HomePageTemp({super.key});
+
+  final opciones = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco'];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Componentes Temp'),
+        title: const Text('Componentes Temp'),
       ),
       
       body: ListView(
-        children: const [
-          ListTile(
-            title: Text('List Title'),
-          ),
-          Divider(),
-
-          ListTile(
-            title: Text('List Title'),
-          ),
-          Divider(),
-
-          ListTile(
-            title: Text('List Title'),
-          ),
-        ],
+        children: _crearItems()
       ),
     );
   }
+
+  List<Widget> _crearItems() {
+    List<Widget> lista = <Widget>[];
+
+    for (String opt in opciones) {
+      final tempWidget = ListTile(
+        title: Text(opt)
+      );
+
+      lista.add(tempWidget);
+      lista.add( const Divider());
+    }
+
+    return lista;
+  }
+
 }
